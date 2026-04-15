e3f04b132b6382cc5214a77c80d6c8a94c533683
Sudip Das <sudipkgec23@gmail.com>
2026-04-16 03:26:05 +0530

    nodemailer fixed bug

--- Backend/src/modules/auth/utils/mailer.js
+++ Backend/src/modules/auth/utils/mailer.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 587,
+  port: 587,
   secure: false,
   auth: {
     user: process.env.SMTP_USER,
@@ -12,8 +12,6 @@ const transporter = nodemailer.createTransport({
     rejectUnauthorized: false,
   },
   connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
 function generateOtpTemplate(otp) {
